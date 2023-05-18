9849edfac76975478fb059de25f1c45f27a5a6c6
ValeOlivares <valentinaolivars85@gmail.com>
2023-05-17 20:58:34 -0400

    feat(variables): add annotations examples

--- variables.ts
+++ variables.ts
@@ -28,3 +28,30 @@ let point:{ x: number; y: number} = {
 const logNumber:(i:number)=> void = (i:number) => {
   console.log(i)
 }
+
+// When to use annotations
+
+// 1. Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates : { x: number, y: number} = JSON.parse(json);
+console.log(coordinates); // { x: 10, y:20}
+
+// 2. When we declare a var on one line and initalizate later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for ( let i = 0; i < words.length; i++){
+  if(words[i] === 'green'){
+    foundWord = true;
+  }
+}
+
+// 3. Var whose type cannot be inferred correctly
+let numbers2= [ -10, -2, 12];
+let numberAboveZero: boolean | number = false;
+
+for ( let i = 0; i < numbers2.length; i++){
+  if(numbers2[i] > 0){
+    numberAboveZero = numbers[i];
+  }
+}
